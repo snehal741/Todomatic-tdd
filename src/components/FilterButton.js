875d4beca85c61bc6c1318cee1a00212bae46406
Snehal <snehalpb01@gmail.com>
2022-07-01 01:39:59 +0530

    first commit

---
+++ src/components/FilterButton.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function FilterButton(Props){
+    return (
+        <button 
+            type="button" 
+            className="btn toggle-btn" 
+            aria-pressed={Props.isPressed}
+            onClick = {()=> Props.setFilter(Props.name)}>
+            <span className="visually-hidden">Show </span>
+            <span>{Props.name}</span>
+            <span className="visually-hidden"> tasks</span>
+        </button>
+    )
+}
+
+export default FilterButton;
